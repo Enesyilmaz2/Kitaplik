5f82857d2f2973d8bb237b07b2c31854a3606f64
enesyilmaz2 <enesyilmaz1905@gmail.com>
2023-10-20 14:49:15 +0300

    ilk commit

---
+++ Kitaplik/Controllers/BookController.cs
@@ -0,0 +1,53 @@
+﻿using Kitaplik.Models;
+using Kitaplik.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kitaplik.Controllers;
+
+public class BookController : Controller
+{
+
+    // Dependency Injection
+    private readonly RepositoryBaglantisi _repository;
+
+    public BookController(RepositoryBaglantisi repository)
+    {
+        _repository = repository;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    // Attribute
+    [HttpPost]
+    public  IActionResult Create(Book book)
+    {
+        _repository.Add(book);
+        _repository.SaveChanges();
+        return RedirectToAction("Index", "Home");
+    }
+
+    //[HttpGet]
+    //public IActionResult Linq()
+    //{
+    //    string[] isimler = { "ras", "dslkj", "afkda", "çömdf", "lkmsdçöns", "roakqmnm" };
+    //    List<string> linqQuery = isimler.Where(x=>x.Contains("r")).ToList();
+
+        
+    //    return View();
+    //}
+
+    public IActionResult GetList()
+    {
+        List<Book> books= _repository.Books.ToList();
+        return View(books);
+    }
+}
